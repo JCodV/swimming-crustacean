ddd836c55aa4f4efb495ca5a29ef76f7b9e869e1
JCodV <154752793+JCodV@users.noreply.github.com>
2024-12-09 00:37:58 -0500

    added flappy bird movement/physics

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
 const WINDOW_WIDTH: f32 = 512.0;
 const WINDOW_HEIGHT: f32 = 600.0;
-
+const GRAVITY_MAX: f32 = 10.0;
 fn main() {
     App::new()
         .add_plugins(
@@ -32,11 +32,9 @@ struct Gravity(f32);
 
 impl Default for Gravity {
     fn default() -> Gravity {
-        Gravity(2.0)
+        Gravity(-1.0)
     }
 }
-#[derive(Component)]
-struct IsJumping(bool);
 
 #[derive(Component)]
 struct Velocity(Vec2);
@@ -44,15 +42,10 @@ struct Velocity(Vec2);
 #[derive(Component)]
 struct JumpVelocity(f32);
 
-#[derive(Component)]
-struct OnGround(bool);
-
 #[derive(Bundle)]
 struct PlayerPhysics {
     velocity: Velocity,
     gravity: Gravity,
-    is_jumping: IsJumping,
-    on_ground: OnGround,
     jump_velocity: JumpVelocity,
 }
 
@@ -61,9 +54,7 @@ impl Default for PlayerPhysics {
         PlayerPhysics {
             velocity: Velocity(Vec2 { x: 0.0, y: 0.0 }),
             gravity: Gravity::default(),
-            is_jumping: IsJumping(false),
-            on_ground: OnGround(false),
-            jump_velocity: JumpVelocity(10.0),
+            jump_velocity: JumpVelocity(80.0),
         }
     }
 }
@@ -82,49 +73,37 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn handle_player_jump(
     keys: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(
-        &mut Transform,
-        &mut Velocity,
-        &mut OnGround,
-        &mut IsJumping,
-        &JumpVelocity,
-        &Rustacean,
-    )>,
-    time: Res<Time>,
+    mut query: Query<(&mut Velocity, &JumpVelocity, &Rustacean)>,
 ) {
-    for (mut transform, mut velocity, mut on_ground, mut is_jumping, jump_velocity, _rustacean) in
-        &mut query
-    {
-        if keys.pressed(KeyCode::Space) && on_ground.0 == true {
+    for (mut velocity, jump_velocity, _rustacean) in &mut query {
+        if keys.just_pressed(KeyCode::Space) {
             velocity.0.y = jump_velocity.0;
-            on_ground.0 = false;
-            is_jumping.0 = true;
-            transform.translation.y += velocity.0.y * time.delta_secs();
         }
     }
 }
 
 fn apply_gravity(
-    mut query: Query<(
-        &mut Transform,
-        &mut Velocity,
-        &mut OnGround,
-        &Gravity,
-        &Rustacean,
-    )>,
+    mut query: Query<(&mut Transform, &mut Velocity, &mut Gravity, &Rustacean)>,
     time: Res<Time>,
 ) {
-    for (mut transform, mut velocity, on_ground, gravity, _rustacean) in &mut query {
-        if on_ground.0 == false {
-            velocity.0.y += gravity.0;
-            transform.translation.y -= velocity.0.y * time.delta_secs();
+    for (mut transform, mut velocity, mut gravity, _rustacean) in &mut query {
+        if gravity.0 > GRAVITY_MAX {
+            gravity.0 = GRAVITY_MAX;
         }
+        velocity.0.y += gravity.0;
+        transform.translation.y += velocity.0.y * time.delta_secs();
     }
 }
 
 #[derive(Component)]
 struct FloorTile;
 
-fn set_environment(mut commands: Commands) {
-    let num_sand_tiles: u32 = WINDOW_WIDTH as u32 / 32;
+fn set_environment(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let num_sand_tiles = WINDOW_WIDTH as i32;
+    for i in 0..num_sand_tiles {
+        commands.spawn((
+            Sprite::from_image(asset_server.load("sand_floor_tile.png")),
+            FloorTile,
+        ));
+    }
 }
