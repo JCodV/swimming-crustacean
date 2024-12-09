dc08abca6f4e17a88acb1cc4605415f4e9ed5bbe
JCodV <154752793+JCodV@users.noreply.github.com>
2024-12-08 23:44:50 -0500

    removed rigidbody component, added playerphysics bundle, added early implementation of gravity and player jump, added sprites for the rustacean and sand floor tiles, removed rigidbodyplugin

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,130 @@
 use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
+const WINDOW_WIDTH: f32 = 512.0;
+const WINDOW_HEIGHT: f32 = 600.0;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(RigidBodyPlugin)
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Swimming Rustacean".into(),
+                        resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, setup)
+        .add_systems(Update, handle_player_jump)
+        .add_systems(Update, apply_gravity)
         .run();
 }
 
-pub struct RigidBodyPlugin;
-impl Plugin for RigidBodyPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
-        app.add_systems(
-            Update,
-            (
-                handle_jump.run_if(input_just_pressed(KeyCode::Space)),
-                apply_gravity,
-            ),
-        );
-    }
-}
+#[derive(Component)]
+struct Rustacean;
 
-#[derive(Resource)]
+#[derive(Component)]
 struct Gravity(f32);
 
+impl Default for Gravity {
+    fn default() -> Gravity {
+        Gravity(2.0)
+    }
+}
 #[derive(Component)]
-struct Crustacean;
-
-#[derive(Component)]
-struct Position(Vec2);
+struct IsJumping(bool);
 
 #[derive(Component)]
 struct Velocity(Vec2);
 
 #[derive(Component)]
-struct Speed(f32);
+struct JumpVelocity(f32);
 
 #[derive(Component)]
 struct OnGround(bool);
 
-#[derive(Component)]
-struct RigidBodyBundle {
-    position: Position,
+#[derive(Bundle)]
+struct PlayerPhysics {
     velocity: Velocity,
-    speed: Speed,
+    gravity: Gravity,
+    is_jumping: IsJumping,
     on_ground: OnGround,
+    jump_velocity: JumpVelocity,
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(Camera2d);
-
-    let shapes = [
-        meshes.add(Circle::new(50.0)),
-        meshes.add(CircularSector::new(50.0, 1.0)),
-    ];
-
-    let num_shapes = shapes.len();
-
-    for (i, shape) in shapes.into_iter().enumerate() {
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
-        commands.spawn((
-            Mesh2d(shape),
-            MeshMaterial2d(materials.add(color)),
-            Transform::from_xyz(
-                // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
-                -900. / 2. + i as f32 / (num_shapes - 1) as f32 * 900.,
-                0.0,
-                0.0,
-            ),
-        ));
-    }
-}
-
-fn add_player(mut commands: Commands) {
-    let player_pos = Vec2::new(0.0, 0.0);
-    let player_vel = Vec2::new(0.0, 0.0);
-    let player_speed: f32 = 100.0;
-    commands
-        .spawn(RigidBodyBundle {
-            position: Position(player_pos),
-            velocity: Velocity(player_vel),
-            speed: Speed(player_speed),
+impl Default for PlayerPhysics {
+    fn default() -> PlayerPhysics {
+        PlayerPhysics {
+            velocity: Velocity(Vec2 { x: 0.0, y: 0.0 }),
+            gravity: Gravity::default(),
+            is_jumping: IsJumping(false),
             on_ground: OnGround(false),
-        })
-        .insert(Crustacean);
-}
-
-// handles jump to all rigid body crustaceans (aka player)
-fn handle_jump(mut query: Query<&mut RigidBodyBundle, With<Crustacean>>) {
-    for mut rigid_body in &mut query {
-        if rigid_body.on_ground.0 == true {
-            rigid_body.velocity.0.y = -1.0 * rigid_body.speed.0;
-            rigid_body.on_ground.0 = false;
+            jump_velocity: JumpVelocity(10.0),
         }
     }
 }
 
-// applies gravity to all rigid body crustaceans (aka player)
-fn apply_gravity(gravity: Res<Gravity>, mut query: Query<&mut RigidBodyBundle, With<Crustacean>>) {
-    for mut rigid_body in &mut query {
-        if rigid_body.on_ground.0 == false {
-            rigid_body.velocity.0.y += 1.0 * gravity.0;
-            rigid_body.position.0.y += rigid_body.velocity.0.y;
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Rustacean,
+        Sprite::from_image(asset_server.load("default_rustacean.png")),
+        PlayerPhysics::default(),
+    ));
+}
+
+fn handle_player_jump(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(
+        &mut Transform,
+        &mut Velocity,
+        &mut OnGround,
+        &mut IsJumping,
+        &JumpVelocity,
+        &Rustacean,
+    )>,
+    time: Res<Time>,
+) {
+    for (mut transform, mut velocity, mut on_ground, mut is_jumping, jump_velocity, _rustacean) in
+        &mut query
+    {
+        if keys.pressed(KeyCode::Space) && on_ground.0 == true {
+            velocity.0.y = jump_velocity.0;
+            on_ground.0 = false;
+            is_jumping.0 = true;
+            transform.translation.y += velocity.0.y * time.delta_secs();
         }
     }
+}
+
+fn apply_gravity(
+    mut query: Query<(
+        &mut Transform,
+        &mut Velocity,
+        &mut OnGround,
+        &Gravity,
+        &Rustacean,
+    )>,
+    time: Res<Time>,
+) {
+    for (mut transform, mut velocity, on_ground, gravity, _rustacean) in &mut query {
+        if on_ground.0 == false {
+            velocity.0.y += gravity.0;
+            transform.translation.y -= velocity.0.y * time.delta_secs();
+        }
+    }
+}
+
+#[derive(Component)]
+struct FloorTile;
+
+fn set_environment(mut commands: Commands) {
+    let num_sand_tiles: u32 = WINDOW_WIDTH as u32 / 32;
 }
